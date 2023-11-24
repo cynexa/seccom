3e4810072ee3ec392fc816e5125ca12b370b3c18
Nex <stepnexx@gmail.com>
2023-11-25 04:41:21 +0530

    final-test

--- script.js
+++ script.js
@@ -1,8 +1,15 @@
- // Timetable JS
-  function showTimetable(day) {
-    const timetables = document.querySelectorAll('.timetable');
-    timetables.forEach(timetable => timetable.style.display = 'none');
+// Timetable JS
+function showTimetable(day) {
+  const timetables = document.querySelectorAll('.timetable');
+  timetables.forEach(timetable => timetable.style.display = 'none');
 
-    const selectedTimetable = document.getElementById(day);
-    selectedTimetable.style.display = 'block';
+  const selectedTimetable = document.getElementById(day);
+  selectedTimetable.style.display = 'block';
+
+  // Add a class to the selected timetable
+  selectedTimetable.classList.add('open');
 }
+document.addEventListener("DOMContentLoaded", function() {
+  // Assuming your Monday timetable has an ID of 'Monday'
+  showTimetable('day1');
+});
